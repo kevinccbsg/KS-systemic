17cde4261c039524b22449896dd8067750513394
kevinccbsg <kevinccbsg@gmail.com>
2021-05-11 01:02:45 +0200

    feat: lesson 4

--- components/routes/api-routes.js
+++ components/routes/api-routes.js
@@ -1,13 +1,35 @@
+const {
+  CustomErrorTypes,
+  errorFactory,
+  handleHttpError,
+  tagError,
+} = require('error-handler-module');
+
+const badRequestError = errorFactory(CustomErrorTypes.BAD_REQUEST);
+const notFoundError = errorFactory(CustomErrorTypes.NOT_FOUND);
+
 module.exports = () => {
-  const start = async ({ app }) => {
-    app.get('/me', (req, res) => {
-      const me = {
-        firstName: 'Kevin',
-        lastName: 'Martinez',
-        age: 27,
-      };
-      res.json(me);
+  const start = async ({ app, logger }) => {
+    app.get('/me', (req, res, next) => {
+      try {
+        const me = {
+          firstName: 'Kevin',
+          lastName: 'Martinez',
+          age: 27,
+        };
+        if (!req.query.age) {
+          throw badRequestError('age is required');
+        }
+        if (me.age < req.query.age) {
+          throw notFoundError('I am not old');
+        }
+        return res.json(me);
+      } catch (err) {
+        return next(tagError(err));
+      }
     });
+
+    app.use(handleHttpError(logger));
 
     return Promise.resolve();
   };
--- test/api.test.js
+++ test/api.test.js
@@ -17,15 +17,18 @@ describe('API Tests', () => {
     .get('/hello-world')
     .expect(200));
 
-  it('"/me" should return 200 (OK)', () => request
+  it('"/me" should return 400 (BAD_REQUEST) if we do not send age', () => request
     .get('/me')
-    .expect(200)
+    .expect(400)
     .then(response => {
-      const expected = {
-        firstName: 'Kevin',
-        lastName: 'Martinez',
-        age: 27,
-      };
-      expect(response.body).to.eql(expected);
+      expect(response.body.message).to.eql('age is required');
+    }));
+
+  it('"/me" should return 404 (NOT_FOUND)', () => request
+    .get('/me')
+    .query({ age: 40 })
+    .expect(404)
+    .then(response => {
+      expect(response.body.message).to.eql('I am not old');
     }));
 });
